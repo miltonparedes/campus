e375860a3c17ffb2436f219946c3f7f4b1b7f71e
Milton Olaf Paredes <miltonolaf@outlook.com>
2019-02-07 21:28:48 -0600

    examp. include app view

--- campus/settings.py
+++ campus/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig',
-    'subjects.apps.SubjectsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- campus/urls.py
+++ campus/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('users/', include('users.urls')),
-    path('subjects/', include('subjects.urls')),
+    path('users/', include('campus.users.urls')),
     path('admin/', admin.site.urls),
 ]
