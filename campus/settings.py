82c1d9a0d9eca46401bc9529866bc7f999ee7a3a
Isaac <izaak343@gmail.com>
2019-02-09 20:34:42 -0600

    first try adding template

--- campus/settings.py
+++ campus/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'campus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            (os.path.join(BASE_DIR, 'templates')),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- campus/users/views.py
+++ campus/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("USERS")
-# Create your views here.
+    template = loader.get_template('base.html')
+    return HttpResponse(template.render())
