50dd6bdeb54141666f9e3f32ab61afbfa6c0393c
Isaac <izaak343@gmail.com>
2019-02-27 21:52:52 -0600

    users initialize

--- modules/users/urls.py
+++ modules/users/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add', views.add, name='add'),
+    path('edit', views.edit, name='edit'),
+    path('delete', views.delete, name='delete')
 ]
--- modules/users/views.py
+++ modules/users/views.py
@@ -6,3 +6,18 @@ from django.template import loader
 def index(request):
     template = loader.get_template('users/index.html')
     return HttpResponse(template.render())
+
+
+def add(request):
+    template = loader.get_template('users/add.html')
+    return HttpResponse(template.render())
+
+
+def edit(request):
+    template = loader.get_template('users/edit.html')
+    return HttpResponse(template.render())
+
+
+def delete(request):
+    template = loader.get_template('users/delete.html')
+    return HttpResponse(template.render())
