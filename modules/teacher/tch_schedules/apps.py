f5c07425e8af7f4fefbb31a4c4336a468744a4a6
Isaac <izaak343@gmail.com>
2019-05-28 13:03:13 -0500

    module configuration

---
+++ modules/teacher/tch_schedules/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SchedulesTeachersConfig(AppConfig):
+    name = 'schedules_teachers'
---
+++ modules/teacher/tch_users/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UseresTeachersConfig(AppConfig):
+    name = 'useres_teachers'
