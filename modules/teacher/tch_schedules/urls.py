4565bc3551b76f9be436d62008f32d14b9d4249c
jesus <elbauldelocio@gmail.com>
2019-06-13 01:00:07 -0500

    teacher templates

--- modules/teacher/tch_schedules/urls.py
+++ modules/teacher/tch_schedules/urls.py
@@ -4,7 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add', views.add, name='add'),
-    path('edit', views.edit, name='edit'),
-    path('delete', views.delete, name='delete')
 ]
