86c40d408988faba008d2b52b15676fb4ebb870a
Milton Olaf <miltonolaf@outlook.com>
2019-08-31 16:11:35 -0500

    Activate django admin

--- core/settings.py
+++ core/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'templates/'),
         ],
-        'APP_DIRS': False,
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -135,3 +135,5 @@ STATICFILES_FINDERS = [
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
+
+LOGIN_REDIRECT_URL = '/'
--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('modules.login.urls')),
     path('users/', include('modules.users.urls')),
     path('courses/', include('modules.courses.urls')),
