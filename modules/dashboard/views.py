7d7e99ff55c2d5e5d864264bd0e65cc08b72683d
Milton Olaf <miltonolaf@outlook.com>
2019-09-01 23:38:09 -0500

    Rendering request

--- modules/dashboard/views.py
+++ modules/dashboard/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 
 
 @login_required
 def index(request):
-    template = loader.get_template('dashboard/index.html')
-    context = {"dashboard_page": "active"}
-    return HttpResponse(template.render(context))
+    return render(
+        request,
+        'dashboard/index.html',
+    )
